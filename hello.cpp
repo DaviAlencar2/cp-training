63a671e9ce2b0c7d6dcb5e7824ed45099b5c4561
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-03-03 23:25:17 -0300

    first commit

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+// iniciando estudos
+
+int main() 
+{
+    int var = 100;
+    cout << "num: " << var << endl;
+    cout << "..." << endl;
+    
+    // leitura
+    int reading1, reading2;
+    
+    cin >> reading1 >> reading2;
+    cout << reading1 << "," << reading2 << endl;
+    
+    // condicional
+    
+    
+    return 0;
+} 
