93ec8ddb32a3469c1cef5d4a9ce52d3f0ae166b2
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-03-03 23:44:45 -0300

    Continuing syntax studies

--- hello.cpp
+++ hello.cpp
@@ -9,12 +9,26 @@ int main()
     cout << "..." << endl;
     
     // leitura
-    int reading1, reading2;
+    int reading1;
+    string reading2;
     
     cin >> reading1 >> reading2;
     cout << reading1 << "," << reading2 << endl;
     
     // condicional
+    
+    if (reading1 % 2 == 0){
+      cout << "reading1 -> par" << endl;
+    }
+    else{
+      cout << "reading1 -> impar" << endl;
+    }
+    
+    // repeticao
+    
+    for (int i = 0; i<reading2.size(); i++){
+      cout << reading2[i] << endl;
+    }
     
     
     return 0;
